3815ebc29783453d525a45c99a92dc45a9e8411a
gorry <gorry@localhost.localdomain>
2021-03-09 17:51:12 +0100

    ldaps

--- ldap20_ldaps/install.sh
+++ ldap20_ldaps/install.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
-# @edt ASIX M06 2018-2019
-# instal.lacio slapd edt.org
+# Install ldap server
 # -------------------------------------
-cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
-cp /opt/docker/cacert.pem /etc/openldap/certs/cacert.pem
-cp /opt/docker/servercert.pem /etc/openldap/certs/servercert.pem
-cp /opt/docker/serverkey.pem  /etc/openldap/certs/serverkey.pem
+
+mkdir /etc/openldap/certs
+cp /opt/docker/cacert.pem /etc/openldap/certs/.
+cp /opt/docker/servercert.pem /etc/openldap/certs/.
+cp /opt/docker/serverkey.pem /etc/openldap/certs/.
 
 rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
-cp /opt/docker/DB_CONFIG /var/lib/ldap
-slaptest -F /etc/openldap/slapd.d -f /opt/docker/slapd.conf
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.ldif
+cp /opt/docker/DB_CONFIG /var/lib/ldap/.
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
+slapadd -F /etc/openldap/slapd.d -l edt.org.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /var/lib/ldap
+cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 
